6a37ae789a3ce0c32d45fbac9d405cec826df4ef
Glassay <31647920+Glassay@users.noreply.github.com>
2017-12-07 20:58:06 +0800

    react 练习

--- pratice-demo/src/index.js
+++ pratice-demo/src/index.js
@@ -1,8 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/**
+ * 2017-12-07 Jifeng Cheng
+ */
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react';
+import reactDOM from 'react-dom';
+
+class Content extends React.Component {
+  render() {
+    const content = 'Sara'
+    const className = 'header'
+    const isGood = false
+    const title = <h1 className='title'>ScriptOJ</h1>
+    const page = <div className='content'></div>
+
+    return(
+      <div>
+        {page} {title}
+        <h1>hello</h1>
+
+        <h1>hello,{content}</h1>
+
+        <h1>1+2={1+2}</h1>
+
+        <h1>{(
+          function() {
+            return content
+          }
+          )()}
+        </h1>
+        
+        <h1 className='className'>
+          <h1>hello,React</h1>
+        </h1>
+
+        <h1>
+          hello,{
+            isGood ? 'Sara' : 'Tina'
+          }
+        </h1>
+      </div>
+    )
+  }
+}
+
+reactDOM.render(
+  <Content />,
+  document.getElementById('root')
+)
